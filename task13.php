dd98fc3a2c6780020727a7644556b0e65ec3832d
Rishu Raj <rishuraj17jan@gmail.com>
2024-09-10 11:11:20 +0530

    php array concept

---
+++ task13.php
@@ -0,0 +1,21 @@
+<?php
+
+    $arr = array("Sophia"=>"31","Jacob"=>"41","william"=>"39","Ramesh"=>"40");
+
+    ksort($arr);
+
+
+    foreach($arr as $x => $x_value){
+        echo "key=".$x.", value".$x_value;
+        echo "<br>";
+    }
+
+    $inventory = [
+        ["item_id" => 1,"item_name" => "Laptop"],
+        ["item_id" => 2,"item_name" => "Monitor"],
+        ["item_id" => 3,"item_name" => "Keyboard"] 
+    ];
+
+    $search_item = "Monitor";
+
+?>
