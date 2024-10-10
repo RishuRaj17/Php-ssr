3e9c1c15d491aa738b10cee949ebd5ebbb4196b9
Rishu Raj <rishuraj17jan@gmail.com>
2024-10-10 10:02:32 +0530

    concept of filters in php

---
+++ filter.php
@@ -0,0 +1,15 @@
+<?php
+$var = 9950;
+ echo var_dump(filter_var($var,FILTER_VALIDATE_INT));
+ 
+ echo "<br>";
+
+ if(filter_var($var, FILTER_VALIDATE_INT)){
+    echo "<br>$var is Integer.";
+ }else{
+    echo "Integer is not Valid";
+ }
+
+
+
+?>
